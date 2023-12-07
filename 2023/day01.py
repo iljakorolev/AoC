ddc244bc1c7aa01b0ad53333e0feb575a6e669a6
ilja_korolev <ilja.korolev@framery.fi>
2023-12-07 23:16:48 +0200

    d1: second half

--- 2023/day01.py
+++ 2023/day01.py
@@ -1,18 +1,56 @@
-input = "../../AoC_Data/2023/day01/puzzle_input.txt"
+import re
 
-num1 = num2 = ""
-total = 0
+def part1(input):
+    total = 0
 
-with open(input) as file:
-    for line in file:
-        line = line.rstrip()
-        for c in line:
-            if c.isdigit():
-                num1 = c
-                break
-        for c in line[ : :-1]:
-            if c.isdigit():
-                num2 = c
-                break
-        total += int(num1 + num2)
-    print(total)
+    with open(input) as file:
+        for line in file:
+            num = ""
+            line = line.rstrip()
+            for c in line:
+                if c.isdigit():
+                    num += c
+                    break
+            for c in reversed(line):
+                if c.isdigit():
+                    num += c
+                    break
+            total += int(num)
+        print("Part1:", total)
+
+def part2(input):
+    DIGITS = {
+        "one"   : 1,
+        "two"   : 2,
+        "three" : 3,
+        "four"  : 4,
+        "five"  : 5,
+        "six"   : 6,
+        "seven" : 7,
+        "eight" : 8,
+        "nine"  : 9,
+        "1": 1,
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+        "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9
+    }
+    
+    total = 0
+    
+    with open(input) as file:
+        for line in file:
+            line = line.rstrip()
+            newLine = re.findall("(?=([0-9]|one|two|three|four|five|six|seven|eight|nine))", line)
+            total += DIGITS[newLine[0]]*10+DIGITS[newLine[-1]]
+        print("Part2:", total)
+                
+file = "../../AoC_Data/2023/day01/puzzle_input.txt"
+part1(file)
+part2(file)
+       
+    
