f911d3bbf753284decd597fe9228d5d6e0c517eb
ilja_korolev <ilja.korolev@framery.fi>
2023-12-17 18:22:54 +0200

    d3: second half

--- 2023/day03.py
+++ 2023/day03.py
@@ -1,4 +1,5 @@
 import re
+from collections import defaultdict
 
 def get_neighbours(x, y, width, height):
     _out = []
@@ -42,7 +43,48 @@ def part1(input):
                 if (i, j) in neighbours:
                     total += int(match.group())
                     break
-    print(total)    
+    print("Part1:", total)    
+
+def part2(input):
+    neighbours = defaultdict(list)
+    gears = defaultdict(list)
+    total = 0
+    
+    with open(input) as file:
+        # Each line splitted into list
+        lines = file.read().split("\n")
+        width = len(lines[0])
+        height = len(lines)
+        idx = 0
+        # Loop through each line - index each line using enum
+        for lidx, line in enumerate(lines):
+            # Loop through each character of that line - index each char using enum
+            for cidx, c in enumerate(line):
+                # Find only "gear" symbol and get its neighbours coords around the symbol
+                if c == "*":
+                    # Add each symbol neighbours as unique key, even if they are on the same line
+                    idx += 1
+                    neighbours[idx] = get_neighbours(lidx,cidx,width,height)
+    
+    # Loop through each line
+    for i, line in enumerate(lines):
+        # Find number and get it's Object
+        for match in re.finditer(r"\d+", line):
+            # Loop symbol neighbours
+            for key,val in neighbours.items():
+                # Break on first match (comparing coords) if part number is neighbour to a symbol
+                # Append each full number to its unique key
+                for j in range(*match.span()):
+                    if (i, j) in val:
+                        gears[key].append(int(match.group()))
+                        break
+    
+    # Loop through dict and sum up only two numbers of each symbol
+    for nums in gears.values():
+        if len(nums) == 2:
+            total += nums[0]*nums[1]
+    print("Part2:", total)
 
 file = "../../AoC_Data/2023/day03/puzzle_input.txt"
 part1(file)
+part2(file)
